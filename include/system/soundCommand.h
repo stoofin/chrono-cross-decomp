c11d9ce103da0fb41d6e2c75b5e32a9fb1024806
Jonathan Peros <jperos@jenova.games>
2026-03-17 05:13:34 +0000

    Decompile func_8004A53C

--- include/system/soundCommand.h
+++ include/system/soundCommand.h
@@ -27,6 +27,7 @@
 #define SOUND_CMD_9E_RESTORE_CUTSCENE_AUDIO             ( 0x9EU )
 #define SOUND_CMD_9F_SUSPEND_CUTSCENE_AUDIO             ( 0x9FU )
 #define SOUND_CMD_A8_UNK                                ( 0xA8U )
+#define SOUND_CMD_A9_UNK                                ( 0xA9U )
 #define SOUND_COMMAND_UNK_F0                            ( 0xF0U )
 #define SOUND_COMMAND_UNK_F1                            ( 0xF1U )
 
